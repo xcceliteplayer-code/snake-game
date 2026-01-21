ad132b78d5e9a52c03cf73307091419ade3617f1
xcceliteplayer-code <xcceliteplayer@gmail.com>
2026-01-21 07:39:24 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCLRvyfE_ZgG20sZuWVge2baOElVSluvKk",
   authDomain: "snake-game-6d2fe.firebaseapp.com",
   databaseURL: "https://snake-game-6d2fe-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "snake-game-6d2fe",
-  storageBucket: "snake-game-6d2fe.firebasestorage.app",
   messagingSenderId: "881486411840",
   appId: "1:881486411840:web:250ca2d50a9dfbf65007a5"
 };
